3f72bb7b378759e03e6dbf3ee53c65bb87fa765b
Jake Fireside <labountypaul@icloud.com>
2020-10-09 17:39:23 -0500

    update scoring code, combined to one HTML

--- script.js
+++ script.js
@@ -6,12 +6,10 @@ let questionElement = document.getElementById("question");
 const answerButtonsElement = document.getElementById("answer-buttons");
 let timeEl = document.getElementById("timeEl");
 let secondsLeft = 101;
-startButton.addEventListener("click", function () {
-  setTimer();
-});
 
+let timerInterval;
 function setTimer() {
-  let timerInterval = setInterval(function () {
+  timerInterval = setInterval(function () {
     secondsLeft--;
     timeEl.textContent = secondsLeft;
 
@@ -31,6 +29,10 @@ nextButton.addEventListener("click", () => {
 });
 
 function startQuiz() {
+  secondsLeft = 101;
+  if (timerInterval) {
+    clearInterval(timerInterval);
+  }
   startButton.classList.add("hide");
   mixedQuestions = questions.sort(() => Math.random() - 0.5);
   currentQuestionIndex = 0;
@@ -63,10 +65,13 @@ function resetState() {
     answerButtonsElement.removeChild(answerButtonsElement.firstChild);
   }
 }
-
+let lastScore;
 function selectAnswer(e) {
   const selectedButton = e.target;
   const correct = selectedButton.dataset.correct;
+  if (correct !== "true") {
+    secondsLeft -= 10;
+  }
   setStatusClass(document.body, correct);
   Array.from(answerButtonsElement.children).forEach((button) => {
     setStatusClass(button, button.dataset.correct);
@@ -74,9 +79,58 @@ function selectAnswer(e) {
   if (mixedQuestions.length > currentQuestionIndex + 1) {
     nextButton.classList.remove("hide");
   } else {
+    // Game Over
     startButton.innerText = "restart";
     startButton.classList.remove("hide");
+    lastScore = secondsLeft;
   }
+}
+document.getElementById("score-btn").addEventListener("click", viewScores);
+
+function viewScores() {
+  document.getElementById("quiz").classList.add("hide");
+  document.getElementById("scores").classList.remove("hide");
+  document.getElementById("thisscore").innerHTML = lastScore;
+}
+// document.getElementById("initailsubmit").addEventListener("click", saveUserScore);
+
+function saveUserScore() {
+  const initials = document.getElementById("initials").value;
+  if (initials === "" || lastScore === undefined) {
+    return;
+  }
+  document.getElementById("initialAndButton").classList.add("hide");
+  let currentScores = localStorage.getItem("quizScores");
+  if (currentScores === null) {
+    currentScores = "";
+  } else {
+    let scores = currentScores.split(",");
+    let scores2 = [];
+    for (let s = 0; s < scores.length; s += 2) {
+      if (initials !== scores[s]) {
+        scores2.push(initials, scores[s + 1]);
+      }
+    }
+    currentScores = scores2.join(",");
+    // replace value for this user if necessary
+  }
+  if (currentScores !== "") {
+    currentScores += ",";
+  }
+  currentScores += `${initials},${lastScore}`;
+  localStorage.setItem("quizScores", currentScores);
+
+  // display all scores
+  let dl = document.createElement("dl");
+  let scores = currentScores.split(",");
+  for (let s = 0; s < scores.length; s += 2) {
+    let dt = document.createElement("dt");
+    dt.innerHTML = scores[s] + ": " + scores[s + 1];
+    dl.appendChild(dt);
+  }
+  let thisScore = document.getElementById("thisscore");
+  thisScore.innerHTML = "";
+  thisScore.appendChild(dl);
 }
 
 function setStatusClass(element, correct) {
